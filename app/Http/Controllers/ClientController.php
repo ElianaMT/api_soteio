e097830d9ca439c53823115e130643421b5255ec
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-12-08 14:32:05 -0500

    feat:ex02_cadastro de clientes

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
+use App\Traits\HttpResponses;
+use Exception;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ClientController extends Controller
 {
-    //
+    use HttpResponses;
+
+    public function store(Request $request){
+    try{
+        $data = $request->all();
+
+    $request->validate([ 
+    'name' => 'string|required', 
+    'email' => 'email|required|unique:clients', 
+    'date_birth' => 'date_format:Y-m-d|required', 
+    'cpf' => 'string|required|unique:clients', 
+    'address' => 'string|required' 
+]);
+
+$client = Client::create($data);
+
+return $client;
+    }catch(Exception $exception){
+        return $this->error($exception->getMessage(),Response::HTTP_BAD_REQUEST);
+
+    }
+ }
+    
 }
